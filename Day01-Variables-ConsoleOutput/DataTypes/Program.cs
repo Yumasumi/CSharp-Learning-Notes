77129e09472c9c271edf474670f4f0bdaaa2c28b
Yumasumi <yumayaru@gmail.com>
2025-03-29 15:08:27 +0800

    Create Program.cs

---
+++ Day01-Variables-ConsoleOutput/DataTypes/Program.cs
@@ -0,0 +1,25 @@
+// See https://aka.ms/new-console-template for more information
+
+char myFavoriteCharacter = '☺';
+Console.WriteLine(myFavoriteCharacter);
+
+// Operators and Order of Evaluation
+int num1 = 5;
+int num2 = 13;
+
+// 5.3 as int will still be 5
+
+// Concatination
+Console.WriteLine("Age is " + num1);
+
+Console.WriteLine("Addition num1 + num2 =  " + (num1 + num2));
+// Order of Evaluation
+Console.WriteLine("Subtraction num1 + num2 =  " + (num1 - num2));
+Console.WriteLine("Multiplication num1 + num2 =  " + num1 * num2);
+Console.WriteLine("Division num1 + num2 =  " + num1 / num2);
+
+// F9 斷點
+// F5
+// Shift + F9 QuickWatch
+
+Console.ReadKey();
